5d836ffbcf4002fb0bac2bdabd4a338b9043053a
Ger2022 <german.traverzaro@istea.com.ar>
2023-04-02 20:10:39 -0300

    PRACTICA1.4

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML= `
+<details>
+        <summary>Titulo de info importante</summary>
+        <img src="https://juncotic.com/wp-content/uploads/2016/09/animacion-1.gif" alt="gif de programacion">
+        <iframe src="https://es.wikipedia.org/wiki/Wikipedia:Portada" frameborder="0"></iframe>
+        <p>Modern Love is a romance-drama anthology developed by John Carney. The series is based on the New York Times
+            column of the same name, although certain aspects of the stories have been fictionalized</p>
+        <a href="https://www.amazon.com/Modern-Love-Season-1/dp/B0875YB37B">Ver serie</a>
+    </details>
+`;
